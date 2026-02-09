a4403ec33aa1f2e9c75041c2ff0dfc7a4e65f4b4
HannahA-28 <339729345@2260I-L29-031>
2026-02-09 13:18:03 -0500

    On question 1

--- Topic 3 - Console Input/Program.cs
+++ Topic 3 - Console Input/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1. Greetings");
+            string name, response;
+            int currentYear, age;
+            currentYear = 2026;
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hi " + name + ", how old are you?");
+            response = Console.ReadLine();
+            Console.WriteLine("Wow, you are " +  response);
+            Console.WriteLine("If you are " + response + ", that means you were born in " + (currentYear - response));
+            
+
+            Console.WriteLine("2. Adder");
+
+
+            Console.WriteLine("3. Distance");
+
+
+            Console.WriteLine("4. Hypotenuse");
         }
     }
 }
