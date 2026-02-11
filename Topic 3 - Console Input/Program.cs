1406ef2578e74044755b36d06401334b90924c5b
HannahA-28 <339729345@2260I-L29-031>
2026-02-11 13:18:29 -0500

    Starting final question!

--- Topic 3 - Console Input/Program.cs
+++ Topic 3 - Console Input/Program.cs
@@ -4,26 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. Greetings");
-            string name, response;
-            int currentYear, age;
-            currentYear = 2026;
-
-            Console.WriteLine("What is your name?");
-            name = Console.ReadLine();
-            Console.WriteLine("Hi " + name + ", how old are you?");
-            response = Console.ReadLine();
-            Console.WriteLine("Wow, you are " +  response);
-            Console.WriteLine("If you are " + response + ", that means you were born in " + (currentYear - response));
-            
-
-            Console.WriteLine("2. Adder");
+            //Console.WriteLine("1. Greetings");
+            //string name, response;
+            //int currentYear = DateTime.Now.Year;
+            //Console.WriteLine("What is your name?");
+            //name = Console.ReadLine();
+            //Console.WriteLine("Hi " + name + ", how old are you?");
+            //response = Console.ReadLine();
+            //Console.WriteLine("Wow, you are " + response);
+            //int age = Convert.ToInt32(response);
+            //int birthYear = currentYear - age;
+            //Console.WriteLine("Which means you were born in: " +  birthYear);
 
 
-            Console.WriteLine("3. Distance");
+            //Console.WriteLine("2. Adder");
+            //string responseOne, responseTwo, responseThree;
+            //Console.WriteLine("Hello, pick any number");
+            //responseOne = Console.ReadLine();
+            //Console.WriteLine("Okay, pick a second number");
+            //responseTwo = Console.ReadLine();
+            //Console.WriteLine("Now pick your last number");
+            //responseThree = Console.ReadLine();
+            //Console.WriteLine("Now I am going to add your numbers...");
+            //int num1 = Convert.ToInt32(responseOne);
+            //int num2 = Convert.ToInt32(responseTwo);
+            //int num3 = Convert.ToInt32(responseThree);
+            //int sum = num1 + num2 + num3;
+            //Console.WriteLine("The sum of your numbers is: " + sum);
+
+
+            //Console.WriteLine("3. Distance");
+            //string response1, response2, response3;
+            //Console.WriteLine("Hello, pick any decimal number");
+            //response1 = Console.ReadLine();
+            //Console.WriteLine("Pick another decimal number");
+            //response2 = Console.ReadLine();
+            //Console.WriteLine("Pick your last decimal number");
+            //response3 = Console.ReadLine();
+            //Console.WriteLine("I will now find the average of your numbers");
+            //decimal number1 = Convert.ToDecimal(response1);
+            //decimal number2 = Convert.ToDecimal(response2);
+            //decimal number3 = Convert.ToDecimal(response3);
+            //decimal total = (number1 + number2 + number3) / 3;
+            //Console.WriteLine("Your total number in km is: " + total + ("km"));
 
 
             Console.WriteLine("4. Hypotenuse");
+
         }
     }
 }
